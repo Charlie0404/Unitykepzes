bb654b0cbbc634e7e4da0d27756ec9b67d5a1443
VK <vasaskaroly0404@gmail.com>
2022-10-25 21:01:19 +0200

    456

--- Assets/First Game/Shooter.cs
+++ Assets/First Game/Shooter.cs
@@ -3,16 +3,51 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] GameObject projectilePrototype;
+
+    enum ShootingPattern 
+    {
+       Random, Sequence 
+   
+    }
+
+
+    [SerializeField] GameObject [] projectilePrototypes;
     [SerializeField] float speed;
 
+    [SerializeField] ShootingPattern pattern; // még egy lehetõséget felvenni és valamilyen billentyûvel tudjak lõni , 1es bullet egyes gomb de ugyanúgy space el fire
+    [SerializeField] KeyCode[] keys; // pl 
+    
+
+
+    int count = 0; 
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            GameObject proto;
 
-            GameObject projectile = Instantiate(projectilePrototype);
+            if (pattern == ShootingPattern.Random)
+
+            {
+
+
+                int randomNum = Random.Range(0, projectilePrototypes.Length);
+                proto = projectilePrototypes[randomNum];
+            }
+
+            else
+            {
+                int index = count % projectilePrototypes.Length;
+
+                proto = projectilePrototypes[index];
+            }
+
+
+                                         
+                      
+            
+            GameObject projectile = Instantiate(proto);
 
             projectile.transform.position = transform.position;
 
@@ -23,10 +58,11 @@ public class Shooter : MonoBehaviour
             /* Vector3 v = transform.TransformVector(Vector3.up);  // sajátból világ koordinátába alakítja
              Vector3 v2 = transform.InverseTransformVector(v);     // transform.TransformPoint // globálbol lokális. mindent tart.
              */
+
             direction.Normalize();
 
             rb.velocity = direction * speed;
-        
+            count++;
         }
     }
 }
